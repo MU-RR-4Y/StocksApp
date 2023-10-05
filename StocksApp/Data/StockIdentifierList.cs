c03f5177446c3fd65fb84d7b4ffaad6e7758082f
MU-RR-4Y <mmurray24@hotmail.com>
2023-10-05 12:36:59 +0100

    Altered stock list to the api limit of 50 per call. added stocklist table pagination

---
+++ StocksApp/Data/StockIdentifierList.cs
@@ -0,0 +1,61 @@
+﻿namespace StocksApp.Data
+{
+    public class StockIdentifierList
+    {
+
+        public string SymbolString = "MMM%2CADBE%2CAMD%2CGOOGL%2CGOOG%2CAMZN%2CAXP%2CAMGN%2CAAPL%2CAMAT%2CADP%2CBA%2CBKNG%2CAVGO%2CCAT%2CCVX%2CCSCO%2CKO%2CCMCSA%2CCOST%2CGILD%2CGS%2CHD%2CHON%2CINTC%2CIBM%2CINTU%2CJNJ%2CJPM%2CMCD%2CMRK%2CMETA%2CMSFT%2CMDLZ%2CNFLX%2CNKE%2CNVDA%2CPEP%2CPG%2CQCOM%2CCRM%2CSBUX%2CTSLA%2CTXN%2CTMUS%2CUNH%2CVZ%2CV%2CWMT%2CDIS";
+
+        public string[] SymbolList = {
+            "MMM",
+            "ADBE",
+            "AMD",
+            "GOOGL",
+            "GOOG",
+            "AMZN",
+            "AXP",
+            "AMGN",
+            "AAPL",
+            "AMAT",
+            "ADP",
+            "BA",
+            "BKNG",
+            "AVGO",
+            "CAT",
+            "CVX",
+            "CSCO",
+            "KO",
+            "CMCSA",
+            "COST",
+            "GILD",
+            "GS",
+            "HD",
+            "HON",
+            "INTC",
+            "IBM",
+            "INTU",
+            "JNJ",
+            "JPM",
+            "MCD",
+            "MRK",
+            "META",
+            "MSFT",
+            "MDLZ",
+            "NFLX",
+            "NKE",
+            "NVDA",
+            "PEP",
+            "PG",
+            "QCOM",
+            "CRM",
+            "SBUX",
+            "TSLA",
+            "TXN",
+            "TMUS",
+            "UNH",
+            "VZ",
+            "V",
+            "WMT",
+            "DIS"
+        };
+    }
+}
