3f090b3e1d01f8196bf33cdaee04abe260ea1423
MU-RR-4Y <mmurray24@hotmail.com>
2023-10-11 17:51:15 +0100

    setup tradestock component. able to select portfolio and open window to buy stock. tradestock window reflects cost based on number of units and cash available on portfolio

--- StocksApp/Models/Portfolio.cs
+++ StocksApp/Models/Portfolio.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using StocksApp.Pages.Users;
 using System.ComponentModel.DataAnnotations;
 
 namespace StocksApp.Models
@@ -7,12 +9,16 @@ namespace StocksApp.Models
     {
         
         public int Id {  get; set; }
+        [Required]
         public List<PortfolioStockModel> holdings { get; set; } = new List<PortfolioStockModel>();
+        [Required]
         public List<Order> orders { get; set; } = new List<Order>();
         public double bookValue { get; set; } = 0;
         public double currentValue { get; set; } = 0;
         public double currentPerformance { get; set; } = 0;
         public double cash { get; set; } = 0;
+
+        [Inject] IDbContextFactory<StockAppDbContext> context {  get; set; }
 
         public void CreditCash(double amount)
         {
@@ -33,5 +39,65 @@ namespace StocksApp.Models
         {
             holdings.Add(portfolioStockModel);
         }
+
+        public PortfolioStockModel FindHolding(string symbol)
+        {
+            foreach (var holding in holdings)
+            {
+                if (holding.symbol == symbol) 
+                {
+                    return holding;
+                }
+            }
+            return null;
+        }
+
+        public async void UpdateHoldings()
+        {
+            //loop through each order on portfolio
+            foreach (var order in orders)
+            {
+                var orderStock = order.symbol;
+                //check if stock already has a holding
+                var holding = FindHolding(orderStock);
+
+                // update existing holdings in DB
+                PortfolioStockModel updatedPSM = holding;
+                if (updatedPSM is not null)
+                {
+                    if(order.direction == "buy")
+                    {
+                        updatedPSM.numberofShares += order.numberOfShares;
+                    }else
+                    {
+                        updatedPSM.numberofShares -= order.numberOfShares;
+                    };
+                    using var ctx = context.CreateDbContext();
+                    ctx.PortfolioStockModel.Update(updatedPSM);
+                    await ctx.SaveChangesAsync();
+                }
+                else
+                {
+                    // Add new holding in DB if required
+                    PortfolioStockModel newPSM = new PortfolioStockModel()
+                    {
+                        portfolioId = Id,
+                        shortName = order.shortName,
+                        symbol = order.symbol,
+                        numberofShares = order.numberOfShares,
+                        currency = order.currency,
+                        averagePrice = order.price
+                    };
+                    using var ctx = context.CreateDbContext();
+                    ctx.PortfolioStockModel.Add(newPSM);
+                    await ctx.SaveChangesAsync();
+                }
+
+
+
+            }
+
+        }
+
     }
 }
